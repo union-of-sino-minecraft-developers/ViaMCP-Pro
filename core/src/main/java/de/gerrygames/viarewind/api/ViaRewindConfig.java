fa2b9c78648395bf6f5fc92d4402caed3b6d4100
SlushKo <2558518136@qq.com>
2023-03-12 09:50:09 +0800

    ViaRewind

---
+++ core/src/main/java/de/gerrygames/viarewind/api/ViaRewindConfig.java
@@ -0,0 +1,20 @@
+package de.gerrygames.viarewind.api;
+
+public interface ViaRewindConfig {
+
+	enum CooldownIndicator {
+		TITLE, ACTION_BAR, BOSS_BAR, DISABLED
+	}
+
+	CooldownIndicator getCooldownIndicator();
+
+	boolean isReplaceAdventureMode();
+
+	boolean isReplaceParticles();
+
+	int getMaxBookPages();
+
+	int getMaxBookPageSize();
+
+	int getAnimationDelay();
+}
